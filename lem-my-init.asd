a622e9ef0126a749e93f25e3b33f7783f3cbd88e
madosuki <yurikasamaaikatsu@gmail.com>
2019-02-01 15:54:41 +0900

    change

--- lem-my-init.asd
+++ lem-my-init.asd
@@ -1,12 +1,11 @@
 (defsystem "lem-my-init"
-  :author "Eitaro Fukamachi"
+  :author "madosuki"
   :license "BSD 2-Clause"
   :description "Configurations for lem"
   :serial t
   :depends-on ("lem-trailing-spaces"
                "lem-lisp-mode")
-  :components ((:file "modes/10-vi")
-               (:file "modes/10-paredit")
+  :components ((:file "modes/10-paredit")
                (:file "modes/10-auto-save")
                (:file "modes/10-trailing-spaces")
                (:file "modes/20-lisp-mode")))
--- modes/10-paredit.lisp
+++ modes/10-paredit.lisp
@@ -13,15 +13,17 @@
                 (when (eq (buffer-major-mode buffer) 'lem-lisp-mode:lisp-mode)
                   (change-buffer-mode buffer 'paredit-mode t))))
 
-(define-command delete-next-sexp-on-paredit-mode () ()
-  (if (find 'paredit-mode (buffer-minor-modes (current-buffer)))
-      (kill-sexp)
-      (lem-vi-mode.commands:vi-delete-line)))
-(define-command change-next-sexp-on-paredit-mode () ()
-  (if (find 'paredit-mode (buffer-minor-modes (current-buffer)))
-      (progn
-        (kill-sexp)
-        (lem-vi-mode.core:change-state 'lem-vi-mode.core:insert))
-      (lem-vi-mode.commands:vi-clear-line)))
-(define-key *command-keymap* "D" 'delete-next-sexp-on-paredit-mode)
-(define-key *command-keymap* "C" 'change-next-sexp-on-paredit-mode)
+;; (define-command delete-next-sexp-on-paredit-mode () ()
+;;   (if (find 'paredit-mode (buffer-minor-modes (current-buffer)))
+;;       (kill-sexp)
+;;       (lem-vi-mode.commands:vi-delete-line)))
+;; 
+;; (define-command change-next-sexp-on-paredit-mode () ()
+;;   (if (find 'paredit-mode (buffer-minor-modes (current-buffer)))
+;;       (progn
+;;         (kill-sexp)
+;;         (lem-vi-mode.core:change-state 'lem-vi-mode.core:insert))
+;;       (lem-vi-mode.commands:vi-clear-line)))
+;; 
+;; (define-key *command-keymap* "D" 'delete-next-sexp-on-paredit-mode)
+;; (define-key *command-keymap* "C" 'change-next-sexp-on-paredit-mode)
